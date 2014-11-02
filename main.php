6100862287e3c0c71d17696a5f83cf3f5bcc08a2
De Totty <detotty@gmail.com>
2014-11-02 21:53:55 +0700

    Merapikan
    Merapikan div class

--- main.php
+++ main.php
@@ -2,7 +2,11 @@
    <div id="content" class="content full">
       <div class="yellow-bg background">
         <span class="cat-title">Akakom Dev Center</span>
-        <div class="cat-punch-line"><img src="images/slide-title-border.png" alt="border"/><span> Yang Pertama dan Utama </span><img src="images/slide-title-border.png" alt="border"/></div>
+        <div class="cat-punch-line">
+        	<img src="images/slide-title-border.png" alt="border"/>
+        	<span> Yang Pertama dan Utama </span>
+        	<img src="images/slide-title-border.png" alt="border"/>
+        </div>
       </div>
    </div>
 </div>
